9e4caf9b7197abf9591a0da2a6605d12c607825e
katgarland <katiegarland1458@gmail.com>
2025-03-04 12:57:48 -0600

    Update scroll.js
    updated js to completely hide hamburger wrapper

--- assets/css/scroll.js
+++ assets/css/scroll.js
@@ -5,7 +5,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("wrapper").style.top = "0";
   } else {
-    document.getElementById("wrapper").style.top = "-50px";
+    document.getElementById("wrapper").style.top = "-56px";
   }
   prevScrollpos = currentScrollPos;
 } 
