c2dd563de1860c430dcddf4d8ca643c52ba4a91f
katgarland <katiegarland1458@gmail.com>
2025-03-10 16:05:15 -0500

    updated homescreen
    added animation to hero image; possibly broke contact buttons on mobile, broke scroll-to-nav on mobile
    
    need to figure out how to format fallboxes to display properly at small screen sizes

--- assets/css/scroll.js
+++ assets/css/scroll.js
@@ -5,7 +5,7 @@ window.onscroll = function() {
   if (prevScrollpos > currentScrollPos) {
     document.getElementById("wrapper").style.top = "0";
   } else {
-    document.getElementById("wrapper").style.top = "-56px";
+    document.getElementById("wrapper").style.top = "-50px";
   }
   prevScrollpos = currentScrollPos;
 } 
